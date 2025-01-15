b770540eaec0fceafc0fb65b4c9ac83093c54701
jadenmolwame <jkmmkj528@gmail.com>
2025-01-15 12:38:00 -0800

    currently working navbar

---
+++ env.bat
@@ -0,0 +1,5 @@
+set SECRET_KEY=2asdf
+set GITHUB_CLIENT_ID=Ov23lir3O0gYZMb1A2jG
+set GITHUB_CLIENT_SECRET=464854a6bf089915a3c46aa181b689f1542ab971
+set MONGO_CONNECTION_STRING=mongodb+srv://zensquiddy:COfx584HqxJunYXF@clutser1.4nvbh.mongodb.net/?retryWrites=true&w=majority
+set MONGO_DBNAME=Organizations
