83e3938d3de7e92cd788fb8b62f7b6ebfef84c5f
tylersatterberg <satterbergtyler@gmail.com>
2025-01-24 11:08:26 -0800

    dsfs

--- webapp.py
+++ webapp.py
@@ -22,7 +22,7 @@ app.debug = False #Change this to False for production
 
 
 client = MongoClient(uri)(
-    uri = "mongodb+srv://zensquiddy:COfx584HqxJunYXF@clutser1.4nvbh.mongodb.net/?retryWrites=true&w=majority&appName=Clutser1"
+    uri = "mongodb+srv://zensquiddy:COfx584HqxJunYXF@clutser1.4nvbh.mongodb.net/?retryWrites=true&w=majority&appName=Clutser1",
     ssl=True,
     ssl_cert_reqs=ssl.CERT_NONE
 )
